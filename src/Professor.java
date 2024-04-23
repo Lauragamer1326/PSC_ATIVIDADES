97ab6e6fd278e0a29e77a7d582079d8cd86ce37d
Lauragamer1326 <141660841+Lauragamer1326@users.noreply.github.com>
2024-04-23 09:45:21 -0300

    Update Professor.java

--- src/Professor.java
+++ src/Professor.java
@@ -1,14 +1,18 @@
 import javax.swing.JOptionPane;
 
 public class Professor {
-		private String nome;
-		private int idade;
-		private String email;
-		
-		public Professor() {
-			this.nome = JOptionPane.showInputDialog("Digite o nome do professor:");
-			this.idade = Integer.parseInt("Digite o nome do professor:");
-			this.email = JOptionPane.showInputDialog("Digite o e-mail do professor:");
-		}
+    private String nome;
+    private int idade;
+    private String email;
+    
+    public Professor() {
+        this.nome = JOptionPane.showInputDialog("Digite o nome do professor:");
+        this.idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do professor:"));
+        this.email = JOptionPane.showInputDialog("Digite o e-mail do professor:");
+    }
 
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        JOptionPane.showMessageDialog(null, "Professor cadastrado com sucesso!");
+    }
 }
