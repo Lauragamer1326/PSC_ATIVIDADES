1164b07729891adbba83b572e5972708e5e3ef16
Lauragamer1326 <141660841+Lauragamer1326@users.noreply.github.com>
2024-04-23 09:44:58 -0300

    Update Aluno.java

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,18 +1,22 @@
 import javax.swing.JOptionPane;
 
 public class Aluno {
-		private String nome;
-		private int idade;
-		private int ra;
-		private String email;
-		private String telefone;
-		
-		public Aluno() {
-			this.nome = JOptionPane.showInputDialog("Digite o nome do aluno:");
-			this.idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do aluno:"));
-			this.ra = Integer.parseInt(JOptionPane.showInputDialog("Digite o RA do aluno:"));
-			this.email = JOptionPane.showInputDialog("Digite o e-mail do aluno:");
-			this.telefone = JOptionPane.showInputDialog("Digite o telefone do aluno:");
-		}
-	}
+    private String nome;
+    private int idade;
+    private int ra;
+    private String email;
+    private String telefone;
 
+    public Aluno() {
+        this.nome = JOptionPane.showInputDialog("Digite o nome do aluno:");
+        this.idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do aluno:"));
+        this.ra = Integer.parseInt(JOptionPane.showInputDialog("Digite o RA do aluno:"));
+        this.email = JOptionPane.showInputDialog("Digite o e-mail do aluno:");
+        this.telefone = JOptionPane.showInputDialog("Digite o telefone do aluno:");
+    }
+
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        JOptionPane.showMessageDialog(null, "Bem vindo aluno!");
+    }
+} 
